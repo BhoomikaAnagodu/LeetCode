233bed71b8a1165f576b5184e25eb6ffbb7e4c72
Bhoomika Anagodu <47350195+BhoomikaAnagodu@users.noreply.github.com>
2025-07-15 14:17:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 55.1 MB (77.21%)

---
+++ 0035-search-insert-position/solution.ts
@@ -0,0 +1,14 @@
+function searchInsert(nums: number[], target: number): number {
+   let left = 0;
+   let right = nums.length;
+
+   while(left < right){
+        const mid = Math.floor((left+right)/2);
+        if(nums[mid] < target){
+            left = mid + 1;
+        }else{
+            right = mid;
+        }
+   }
+   return left;
+};
