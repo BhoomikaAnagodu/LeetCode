bd0f42348b359c8d5d5dcabbbc0792fd39ee08ed
Bhoomika Anagodu <47350195+BhoomikaAnagodu@users.noreply.github.com>
2025-07-22 16:47:32 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 54 MB (91.96%)

---
+++ 0119-pascals-triangle-ii/solution.ts
@@ -0,0 +1,10 @@
+function getRow(rowIndex: number): number[] {
+    let res = [];
+    for (let i = 0; i <= rowIndex; i++) {
+        res.unshift(1);
+        for (let j = 1; j < i; j++) {
+            res[j] += res[j + 1];
+        }
+    }
+    return res;
+};
