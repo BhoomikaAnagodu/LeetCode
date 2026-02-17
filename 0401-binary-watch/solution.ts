33b2debacbc88bf6579fedb67dfe4d4cb38df465
Bhoomika Anagodu <47350195+BhoomikaAnagodu@users.noreply.github.com>
2026-02-17 19:44:57 +0000

    [LeetCode Sync] - Binary Watch - Runtime - 4 ms (28.30%), Memory - 58.4 MB (20.75%)

---
+++ 0401-binary-watch/solution.ts
@@ -0,0 +1,11 @@
+function readBinaryWatch(turnedOn: number): string[] {
+    let res = [];
+    for(let h = 0; h < 12; h++){
+        for(let m = 0; m < 60; m++){
+            if(h.toString(2).split("1").length - 1 + (m.toString(2).split("1").length - 1) === turnedOn){
+                res.push(h + ":" + (m < 10 ? "0" : "") + m);
+            }
+        }
+    }
+    return res;
+};
